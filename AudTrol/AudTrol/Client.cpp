07fc7e9e9d65ad83e0020275465562a31a199d12
bis-hanich-79 <bis-hanich-79@greendreamteam.onmicrosoft.com>
2025-09-22 14:51:58 +0300

    wsa finshed trolling me

--- AudTrol/AudTrol/Client.cpp
+++ AudTrol/AudTrol/Client.cpp
@@ -3,14 +3,13 @@
 
 Client::Client(const std::string& server_ip, const std::string& server_port) : m_server_address_info(nullptr)
 {
-	
 	addrinfo hints;
 	ZeroMemory(&hints, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = IPPROTO_TCP;
 	CHECK(WSAStartup(MAKEWORD(2, 2), &m_wsadata), !0, "couldn't start wsa");
-	m_sock = socket(AF_UNSPEC, SOCK_STREAM, IPPROTO_TCP);
+	m_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	CHECK(m_sock, SOCKET_ERROR, "coultn't init sock")
 	CHECK(getaddrinfo(server_ip.c_str(), server_port.c_str(), &hints, &m_server_address_info), true, "couldn't get address info");
 }
@@ -27,9 +26,9 @@ Client::~Client()
 
 std::vector<char> Client::recieve_data(const size_t size) const
 {
-
 	std::vector<char> data(size);
 	CHECK(recv(m_sock, data.data(), size, 0), SOCKET_ERROR, "couldn't recive data");
+	return data;
 }
 
 void Client::send_data(const std::vector<char>& data) const
--- AudTrol/AudTrol/main.cpp
+++ AudTrol/AudTrol/main.cpp
@@ -4,46 +4,24 @@
 
 constexpr char SERVER_PORT[] = "8089";
 constexpr char SERVER_IP[] = "127.0.0.1";
+
+
+
 int main()
 {
-
-	char HOLD[sizeof(in_addr)];
-
-	//memset(&addr, 0, sizeof(sockaddr_in));
-
-	//addr.sin_family = AF_INET;
-	//addr.sin_port = htons(SERVER_PORT);
-	//inet_pton(AF_INET, , &addr.sin_addr.S_un.S_addr);
-	try
-	{
-
-
-	}
-	catch(const std::exception& e)
-	{
-		
-		std::cout << e.what();
-		return WSAGetLastError();
-	}
-
-
-	//std::cout << addr.sin_family << " " << addr.sin_port << " " << addr.sin_addr.S_un.S_addr << std::endl;
-
 	try
 	{
 		Client some_client(SERVER_IP, SERVER_PORT);
  		std::string some_string = "some string";
 		some_client.connect_client();
 		some_client.send_data(some_string.data(), some_string.size());
-		std::vector<char> data = some_client.recieve_data(1500);
-		std::string message = data.data();
-
+		std::vector<char> string = some_client.recieve_data(1500);
+		std::string message = string.data();
 	}
 	catch (const std::exception& e)
 	{
 		std::cout << e.what();
 		return WSAGetLastError();
 	}
-
 }
 
